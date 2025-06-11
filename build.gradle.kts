30ac49bf2c7dc791605509778714d5c1a228a181
kinto-gota-hasegawa <gota.hasegawa@kinto-technologies.com>
2025-06-11 15:26:09 +0900

    Add notebook.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.1.10"
     id("io.ktor.plugin") version "3.1.3"
+    id("org.jetbrains.kotlin.plugin.dataframe") version "1.0.0-dev-7302"
 }
 
 group = "com.example.notebooksample"
@@ -25,4 +26,6 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("org.jetbrains.kotlinx:dataframe:1.0.0-Beta2")
 }
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -4,10 +4,15 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+data class User(val name: String)
+
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/user") {
+            call.respondText("Hello")
         }
     }
 }
